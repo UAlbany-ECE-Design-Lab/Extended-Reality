cfecdf2eed75485c3b68a91c65c9d4a0b77d73b6
ebo0102 <ebo0102@yahoo.com>
2024-04-14 00:42:32 -0400

    Updated server.py

--- Unreal Engine 5 Files/X11823/server.py
+++ Unreal Engine 5 Files/X11823/server.py
@@ -5,12 +5,116 @@ import PySpice
 from PySpice.Spice.Netlist import Circuit
 from PySpice.Unit import *
 
+import matplotlib.pyplot as plt
+from PySpice.Probe.Plot import plot
+
+from scipy.optimize import curve_fit
+
+figure, (ax1, ax2) = plt.subplots(2, figsize=(20, 10))
+
+###################################################################################
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('127.0.0.1',5001))
 
 server.listen()
+"""
+## RC Circuit Example
+#########################################################################################
+print('RC Circuit')
+circuitRC = Circuit('RC')
+simulatorRC = circuitRC.simulator(temperature = 27, nominal_temperature = 27)
 
+source = circuitRC.PulseVoltageSource('input', 'in', circuitRC.gnd,
+                initial_value=0@u_V, pulsed_value=10@u_V,
+                pulse_width=10@u_ms, period=20@u_ms)
+
+circuitRC.R(1, 'in', 'out', 1@u_kΩ)
+element = circuitRC.C
+value = 1@u_uF
+element(1, 'out', circuitRC.gnd, value)
+tau = circuitRC['R1'].resistance * circuitRC['C1'].capacitance
+
+step_time = 10@u_us
+analysis = simulatorRC.transient(step_time=step_time, end_time=source.period*3)
+
+def out_voltage(t, tau):
+    return float(source.pulsed_value) * (1 -  np.exp(-t / tau))
+
+i_max = int(5 * tau / float(step_time))
+popt, pcov = curve_fit(out_voltage, analysis.out.abscissa[:i_max], analysis.out[:i_max])
+tau_measured = popt[0]
+
+print('tau {0} = {1}'.format('capacitor', tau.canonise().str_space()))
+print('tau measured {0} = {1:.1f} ms'.format('capacitor', tau_measured * 1000))
+
+ax = ax1
+title = "Capacitor: voltage is constant"
+
+ax.set_title(title)
+ax.grid()
+current_scale = 1000
+ax.plot(analysis['in'])
+ax.plot(analysis['out'])
+ax.plot(((analysis['in'] - analysis.out)/circuitRC['R1'].resistance) * current_scale)
+ax.axvline(x=float(tau), color='red')
+ax.set_ylim(-11, 11)
+ax.set_xlabel('t [s]')
+ax.set_ylabel('[V]')
+ax.legend(('Vin [V]', 'Vout [V]', 'I'), loc=(.8,.8))
+
+plt.tight_layout()
+plt.show()
+####################################################
+
+## RL Circuit Example
+#########################################################################################
+print('RL Circuit')
+circuitRL = Circuit('RL')
+
+source = circuitRL.PulseVoltageSource('input', 'in', circuitRL.gnd,
+                initial_value=0@u_V, pulsed_value=10@u_V,
+                pulse_width=10@u_ms, period=20@u_ms)
+
+circuitRL.R(1, 'in', 'out', 1@u_kΩ)
+element = circuitRL.L
+value = 1@u_H
+element(1, 'out', circuitRL.gnd, value)
+tau = circuitRL['L1'].inductance / circuitRL['R1'].resistance
+
+simulatorRL = circuitRL.simulator(temperature = 27, nominal_temperature = 27)
+step_time = 10@u_us
+analysis = simulatorRL.transient(step_time=step_time, end_time=source.period*3)
+
+def out_voltage(t, tau):
+    return float(source.pulsed_value) * np.exp(-t / tau)
+
+i_max = int(5 * tau / float(step_time))
+popt, pcov = curve_fit(out_voltage, analysis.out.abscissa[:i_max], analysis.out[:i_max])
+tau_measured = popt[0]
+
+print('tau {0} = {1}'.format('capacitor', tau.canonise().str_space()))
+print('tau measured {0} = {1:.1f} ms'.format('capacitor', tau_measured * 1000))
+
+ax = ax2
+title = "Inductor: current is constant"
+
+ax.set_title(title)
+ax.grid()
+current_scale = 1000
+ax.plot(analysis['in'])
+ax.plot(analysis['out'])
+ax.plot(((analysis['in'] - analysis.out)/circuitRL['R1'].resistance) * current_scale)
+ax.axvline(x=float(tau), color='red')
+ax.set_ylim(-11, 11)
+ax.set_xlabel('t [s]')
+ax.set_ylabel('[V]')
+ax.legend(('Vin [V]', 'Vout [V]', 'I'), loc=(.8,.8))
+
+plt.tight_layout()
+plt.show()
+####################################################
+"""
 
 # Example of node voltage and current
 ####################################################################################
@@ -58,36 +162,6 @@ analysisCurrent = simulatorCurrent.operating_point()
 
 for node in analysisCurrent.branches.values():
     print('Node {}: {:5.4f} mA'.format(str(node), float(node)*10000))
-#************************************************
-############################################################################
-
-# RC Circuit
-
-# figure, (ax1, ax2) = plt.subplots(2, figsize=(20,10))
-
-element_types = ('capacitor', 'inductor')
-"""
-for element_type in ('capacitor', 'inductor'):
-
-    circuit = Circuit(element_type.title())
-
-    source = circuit.PulseVoltageSource('vin','1', '0', initial_val=0@u_V, pulse_val=10@u_V, pulse_width=10@u_ms, period=20@u_ms)
-    circuit.R('1', '1', '2', 1@u_kOhm)
-
-    if element_type == 'capacitor':
-
-        element_type 
-
-
-"""
-
-
-
-
-###############################################################################
-
-# RL Circuit
-
 
 list = []
 
@@ -107,11 +181,94 @@ while True:
         sys.exit()
     else:
         if val == 'VoltageDivider':
+
             client.send(b'Compute: VoltageDivder')
+            circuit = Circuit('Voltage Divider')
+            circuit.V('1', '1', '0', 5@u_V)
+            circuit.R('1', '1', '2', .1@u_kOhm)
+            circuit.R('2', '2', '0', 1@u_kOhm)
+            simulator = circuit.simulator(temprature = 27, nominal_temprature = 27)
+            analysis = simulator.operating_point()
+            print(circuit)
+            print(format_output(analysis))
+
         elif val == 'RCCircuit':
+
             client.send(b'Compute: RCCircuit')
+            print('RC Circuit')
+            circuitRC = Circuit('RC')
+            simulatorRC = circuitRC.simulator(temperature = 27, nominal_temperature = 27)
+            source = circuitRC.PulseVoltageSource('input', 'in', circuitRC.gnd,
+                            initial_value=0@u_V, pulsed_value=10@u_V,
+                            pulse_width=10@u_ms, period=20@u_ms)
+            circuitRC.R(1, 'in', 'out', 1@u_kΩ)
+            element = circuitRC.C
+            value = 1@u_uF
+            element(1, 'out', circuitRC.gnd, value)
+            tau = circuitRC['R1'].resistance * circuitRC['C1'].capacitance
+            step_time = 10@u_us
+            analysis = simulatorRC.transient(step_time=step_time, end_time=source.period*3)
+            def out_voltage(t, tau):
+                return float(source.pulsed_value) * (1 -  np.exp(-t / tau))
+            i_max = int(5 * tau / float(step_time))
+            popt, pcov = curve_fit(out_voltage, analysis.out.abscissa[:i_max], analysis.out[:i_max])
+            tau_measured = popt[0]
+            print('tau {0} = {1}'.format('capacitor', tau.canonise().str_space()))
+            print('tau measured {0} = {1:.1f} ms'.format('capacitor', tau_measured * 1000))
+            ax = ax1
+            title = "Capacitor: voltage is constant"
+            ax.set_title(title)
+            ax.grid()
+            current_scale = 1000
+            ax.plot(analysis['in'])
+            ax.plot(analysis['out'])
+            ax.plot(((analysis['in'] - analysis.out)/circuitRC['R1'].resistance) * current_scale)
+            ax.axvline(x=float(tau), color='red')
+            ax.set_ylim(-11, 11)
+            ax.set_xlabel('t [s]')
+            ax.set_ylabel('[V]')
+            ax.legend(('Vin [V]', 'Vout [V]', 'I'), loc=(.8,.8))
+            plt.tight_layout()
+            plt.show()
+
         elif val == 'RLCircuit':
             client.send(b'Compute: RLCircuit')
+
+            print('RL Circuit')
+            circuitRL = Circuit('RL')
+            source = circuitRL.PulseVoltageSource('input', 'in', circuitRL.gnd,
+                            initial_value=0@u_V, pulsed_value=10@u_V,
+                            pulse_width=10@u_ms, period=20@u_ms)
+            circuitRL.R(1, 'in', 'out', 1@u_kΩ)
+            element = circuitRL.L
+            value = 1@u_H
+            element(1, 'out', circuitRL.gnd, value)
+            tau = circuitRL['L1'].inductance / circuitRL['R1'].resistance
+            simulatorRL = circuitRL.simulator(temperature = 27, nominal_temperature = 27)
+            step_time = 10@u_us
+            analysis = simulatorRL.transient(step_time=step_time, end_time=source.period*3)
+            def out_voltage(t, tau):
+                return float(source.pulsed_value) * np.exp(-t / tau)
+            i_max = int(5 * tau / float(step_time))
+            popt, pcov = curve_fit(out_voltage, analysis.out.abscissa[:i_max], analysis.out[:i_max])
+            tau_measured = popt[0]
+            print('tau {0} = {1}'.format('capacitor', tau.canonise().str_space()))
+            print('tau measured {0} = {1:.1f} ms'.format('capacitor', tau_measured * 1000))
+            ax = ax2
+            title = "Inductor: current is constant"
+            ax.set_title(title)
+            ax.grid()
+            current_scale = 1000
+            ax.plot(analysis['in'])
+            ax.plot(analysis['out'])
+            ax.plot(((analysis['in'] - analysis.out)/circuitRL['R1'].resistance) * current_scale)
+            ax.axvline(x=float(tau), color='red')
+            ax.set_ylim(-11, 11)
+            ax.set_xlabel('t [s]')
+            ax.set_ylabel('[V]')
+            ax.legend(('Vin [V]', 'Vout [V]', 'I'), loc=(.8,.8))
+            plt.tight_layout()
+            plt.show()
 
         elif val[0] == 'V':
             print('Voltage')
